42c3aaaa5dbba8f6246ecf42d5a9e17adf43dbb2
eirik lysenstoen <elysenstoen@gmail.com>
2020-11-10 13:48:04 +0100

    mailing still does not work but added user:reference to beer the form now works

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,20 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def mailer
+    @user = current_user
+    respond_to do |format|
+      if @user
+        # Tell the UserMailer to send a welcome email after save
+        UserMailer.with(user: @user).welcome_email.deliver_later
+
+        format.html { redirect_to(beers_url, notice: 'User was successfully created.') }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 end
